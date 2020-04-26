72408937ea523575456ec65e63911ba28a99a1c7
徐龙彩 <792756411@qq.com>
2020-04-26 23:11:44 +0800

    sds

--- route/route.php
+++ route/route.php
@@ -15,6 +15,7 @@ Route::get('think', function () {
 
 Route::get('hello/:name', 'index/hello');
 Route::any('getPayConfig','pay/getPayParams');
+Route::any('payNotice','pay/payNotice');
 return [
 
 ];
